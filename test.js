bc4d1e9df7be07739fc0cb5ace5be96871dfa7ec
Erdi Köse <me@erdkse.com>
2020-10-23 02:01:18 +0300

    add dotenv and lints

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
+require('dotenv').config();
 const ScrapezoneClient = require('./index');
-const scrapezoneClient = new ScrapezoneClient('bbb', 'aaa');
 
-async function run(){
+const {SCRAPEZONE_USERNAME, SCRAPEZONE_PASSWORD} = process.env;
+
+const scrapezoneClient = new ScrapezoneClient(
+    SCRAPEZONE_USERNAME,
+    SCRAPEZONE_PASSWORD
+);
+
+async function run() {
     const results = await scrapezoneClient.scrape({
-        parser_name: "amazon_product_display",
+        parser_name: 'amazon_product_display',
         query: [
-            "https://amazon.com/dp/B01LSUQSB0",
-            "https://amazon.com/dp/B084K5HNCB"
+            'https://amazon.com/dp/B01LSUQSB0',
+            'https://amazon.com/dp/B084K5HNCB'
         ]
     });
     console.log(results);
 }
 
-run().catch(e => console.error(e));
+run().catch(e => console.error(e.message));
